cd766b4ae60fa992e24fa038928450582b833bcd
thanhhuy25 <nguyenhuythanh256@gmail.com>
2023-10-13 10:58:18 +0700

    Create RJButton

---
+++ GymManagementSystem/FHome.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace GymManagementSystem
+{
+    partial class FHome
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            rjButton1 = new RJButton();
+            SuspendLayout();
+            // 
+            // rjButton1
+            // 
+            rjButton1.BackColor = Color.MediumSlateBlue;
+            rjButton1.BorderColor = Color.Black;
+            rjButton1.BorderRadius = 40;
+            rjButton1.BorderSize = 0;
+            rjButton1.FlatAppearance.BorderSize = 0;
+            rjButton1.FlatStyle = FlatStyle.Flat;
+            rjButton1.ForeColor = Color.White;
+            rjButton1.Location = new Point(310, 136);
+            rjButton1.Name = "rjButton1";
+            rjButton1.Size = new Size(150, 40);
+            rjButton1.TabIndex = 0;
+            rjButton1.Text = "Chicken Huy";
+            rjButton1.UseVisualStyleBackColor = false;
+            // 
+            // FHome
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(rjButton1);
+            Name = "FHome";
+            Text = "FHome";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private RJButton rjButton1;
+    }
+}
