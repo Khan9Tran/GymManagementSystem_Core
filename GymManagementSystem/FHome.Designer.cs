1a53be1b9dc70e72286ded7362c6c0506dbb804e
thanhhuy25 <nguyenhuythanh256@gmail.com>
2023-10-13 14:19:42 +0700

    Reup

--- GymManagementSystem/FHome.Designer.cs
+++ GymManagementSystem/FHome.Designer.cs
@@ -28,51 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
-            this.btnHome = new System.Windows.Forms.Button();
-            this.flowLayoutPanel1.SuspendLayout();
-            this.SuspendLayout();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            btnHome = new Button();
+            flowLayoutPanel1.SuspendLayout();
+            SuspendLayout();
             // 
             // flowLayoutPanel1
             // 
-            this.flowLayoutPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(59)))), ((int)(((byte)(41)))), ((int)(((byte)(55)))));
-            this.flowLayoutPanel1.Controls.Add(this.btnHome);
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
-            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(114, 1440);
-            this.flowLayoutPanel1.TabIndex = 0;
+            flowLayoutPanel1.BackColor = Color.FromArgb(59, 41, 55);
+            flowLayoutPanel1.Controls.Add(btnHome);
+            flowLayoutPanel1.Location = new Point(0, 0);
+            flowLayoutPanel1.Margin = new Padding(0);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(100, 1080);
+            flowLayoutPanel1.TabIndex = 0;
             // 
             // btnHome
             // 
-            this.btnHome.BackColor = System.Drawing.Color.Transparent;
-            this.btnHome.BackgroundImage = global::GymManagementSystem.Properties.Resources.home;
-            this.btnHome.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.btnHome.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnHome.FlatAppearance.BorderSize = 0;
-            this.btnHome.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(99)))), ((int)(((byte)(63)))), ((int)(((byte)(91)))));
-            this.btnHome.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(99)))), ((int)(((byte)(63)))), ((int)(((byte)(91)))));
-            this.btnHome.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnHome.Location = new System.Drawing.Point(23, 27);
-            this.btnHome.Margin = new System.Windows.Forms.Padding(23, 27, 23, 0);
-            this.btnHome.Name = "btnHome";
-            this.btnHome.Size = new System.Drawing.Size(69, 80);
-            this.btnHome.TabIndex = 0;
-            this.btnHome.UseVisualStyleBackColor = false;
+            btnHome.BackColor = Color.Transparent;
+            btnHome.BackgroundImage = Properties.Resources.home;
+            btnHome.BackgroundImageLayout = ImageLayout.Center;
+            btnHome.Cursor = Cursors.Hand;
+            btnHome.FlatAppearance.BorderSize = 0;
+            btnHome.FlatAppearance.MouseDownBackColor = Color.FromArgb(99, 63, 91);
+            btnHome.FlatAppearance.MouseOverBackColor = Color.FromArgb(99, 63, 91);
+            btnHome.FlatStyle = FlatStyle.Flat;
+            btnHome.Location = new Point(23, 27);
+            btnHome.Margin = new Padding(23, 27, 23, 0);
+            btnHome.Name = "btnHome";
+            btnHome.Size = new Size(60, 60);
+            btnHome.TabIndex = 0;
+            btnHome.UseVisualStyleBackColor = false;
             // 
             // FHome
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1924, 1055);
-            this.Controls.Add(this.flowLayoutPanel1);
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.Name = "FHome";
-            this.ShowIcon = false;
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.flowLayoutPanel1.ResumeLayout(false);
-            this.ResumeLayout(false);
-
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1902, 1033);
+            Controls.Add(flowLayoutPanel1);
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "FHome";
+            ShowIcon = false;
+            StartPosition = FormStartPosition.CenterScreen;
+            flowLayoutPanel1.ResumeLayout(false);
+            ResumeLayout(false);
         }
 
         #endregion
