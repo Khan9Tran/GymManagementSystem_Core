63d34a8128fc86b336d8c60bac7ed1714e22c695
thanhhuy25 <nguyenhuythanh256@gmail.com>
2023-10-13 12:57:29 +0700

    Create FHome

--- GymManagementSystem/FHome.Designer.cs
+++ GymManagementSystem/FHome.Designer.cs
@@ -28,38 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
-            rjButton1 = new RJButton();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            flpHome = new FlowLayoutPanel();
+            btnIconHome = new Button();
+            btnHome = new Button();
+            flowLayoutPanel1.SuspendLayout();
+            flpHome.SuspendLayout();
             SuspendLayout();
             // 
-            // rjButton1
+            // flowLayoutPanel1
             // 
-            rjButton1.BackColor = Color.MediumSlateBlue;
-            rjButton1.BorderColor = Color.Black;
-            rjButton1.BorderRadius = 40;
-            rjButton1.BorderSize = 0;
-            rjButton1.FlatAppearance.BorderSize = 0;
-            rjButton1.FlatStyle = FlatStyle.Flat;
-            rjButton1.ForeColor = Color.White;
-            rjButton1.Location = new Point(310, 136);
-            rjButton1.Name = "rjButton1";
-            rjButton1.Size = new Size(150, 40);
-            rjButton1.TabIndex = 0;
-            rjButton1.Text = "Chicken Huy";
-            rjButton1.UseVisualStyleBackColor = false;
+            flowLayoutPanel1.BackColor = Color.FromArgb(59, 41, 55);
+            flowLayoutPanel1.Controls.Add(flpHome);
+            flowLayoutPanel1.Dock = DockStyle.Left;
+            flowLayoutPanel1.Location = new Point(0, 0);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(250, 1041);
+            flowLayoutPanel1.TabIndex = 0;
+            // 
+            // flpHome
+            // 
+            flpHome.BackColor = Color.Transparent;
+            flpHome.Controls.Add(btnIconHome);
+            flpHome.Controls.Add(btnHome);
+            flpHome.Location = new Point(0, 0);
+            flpHome.Margin = new Padding(0);
+            flpHome.Name = "flpHome";
+            flpHome.Size = new Size(250, 65);
+            flpHome.TabIndex = 1;
+            // 
+            // btnIconHome
+            // 
+            btnIconHome.BackColor = Color.Transparent;
+            btnIconHome.BackgroundImage = Properties.Resources.home;
+            btnIconHome.BackgroundImageLayout = ImageLayout.Center;
+            btnIconHome.FlatAppearance.BorderSize = 0;
+            btnIconHome.FlatAppearance.MouseDownBackColor = Color.Transparent;
+            btnIconHome.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            btnIconHome.FlatStyle = FlatStyle.Flat;
+            btnIconHome.Location = new Point(3, 3);
+            btnIconHome.Name = "btnIconHome";
+            btnIconHome.Size = new Size(60, 60);
+            btnIconHome.TabIndex = 0;
+            btnIconHome.UseVisualStyleBackColor = false;
+            btnIconHome.MouseLeave += btnIconHome_MouseLeave;
+            btnIconHome.MouseHover += btnIconHome_MouseHover;
+            // 
+            // btnHome
+            // 
+            btnHome.FlatAppearance.BorderSize = 0;
+            btnHome.FlatAppearance.MouseDownBackColor = Color.Transparent;
+            btnHome.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            btnHome.FlatStyle = FlatStyle.Flat;
+            btnHome.Font = new Font("Times New Roman", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            btnHome.ForeColor = Color.FromArgb(158, 159, 155);
+            btnHome.Location = new Point(69, 3);
+            btnHome.Name = "btnHome";
+            btnHome.Size = new Size(170, 60);
+            btnHome.TabIndex = 1;
+            btnHome.Text = "Home";
+            btnHome.TextAlign = ContentAlignment.MiddleLeft;
+            btnHome.UseVisualStyleBackColor = true;
+            btnHome.MouseLeave += btnHome_MouseLeave;
+            btnHome.MouseHover += btnHome_MouseHover;
             // 
             // FHome
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(rjButton1);
+            ClientSize = new Size(1904, 1041);
+            Controls.Add(flowLayoutPanel1);
             Name = "FHome";
-            Text = "FHome";
+            ShowIcon = false;
+            StartPosition = FormStartPosition.CenterScreen;
+            flowLayoutPanel1.ResumeLayout(false);
+            flpHome.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
 
-        private RJButton rjButton1;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private FlowLayoutPanel flpHome;
+        private Button btnIconHome;
+        private Button btnHome;
     }
 }
--- GymManagementSystem/FHome.cs
+++ GymManagementSystem/FHome.cs
@@ -16,5 +16,33 @@ namespace GymManagementSystem
         {
             InitializeComponent();
         }
+
+        private void btnHome_MouseHover(object sender, EventArgs e)
+        {
+            btnIconHome.BackColor = ColorTranslator.FromHtml("#633f5b");
+            btnHome.BackColor = ColorTranslator.FromHtml("#633f5b");
+            flpHome.BackColor = ColorTranslator.FromHtml("#633f5b");
+        }
+
+        private void btnIconHome_MouseHover(object sender, EventArgs e)
+        {
+            btnIconHome.BackColor = ColorTranslator.FromHtml("#633f5b");
+            btnHome.BackColor = ColorTranslator.FromHtml("#633f5b");
+            flpHome.BackColor = ColorTranslator.FromHtml("#633f5b");
+        }
+
+        private void btnIconHome_MouseLeave(object sender, EventArgs e)
+        {
+            btnIconHome.BackColor = ColorTranslator.FromHtml("#3b2937");
+            btnHome.BackColor = ColorTranslator.FromHtml("#3b2937");
+            flpHome.BackColor = ColorTranslator.FromHtml("#3b2937");
+        }
+
+        private void btnHome_MouseLeave(object sender, EventArgs e)
+        {
+            btnIconHome.BackColor = ColorTranslator.FromHtml("#3b2937");
+            btnHome.BackColor = ColorTranslator.FromHtml("#3b2937");
+            flpHome.BackColor = ColorTranslator.FromHtml("#3b2937");
+        }
     }
 }
