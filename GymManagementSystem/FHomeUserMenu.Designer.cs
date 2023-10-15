3ac09505923961d89e895a21185815756e59bff9
thanhhuy25 <nguyenhuythanh256@gmail.com>
2023-10-16 01:13:24 +0700

    Design FHomeUserMenu

--- GymManagementSystem/FHomeUserMenu.Designer.cs
+++ GymManagementSystem/FHomeUserMenu.Designer.cs
@@ -28,85 +28,267 @@
         /// </summary>
         private void InitializeComponent()
         {
-            flpRegisterMember = new RJFlowLayoutPanel();
-            rjFlowLayoutPanel1 = new RJFlowLayoutPanel();
-            rjFlowLayoutPanel2 = new RJFlowLayoutPanel();
-            rjFlowLayoutPanel3 = new RJFlowLayoutPanel();
+            flpPurchasePackage = new FlowLayoutPanel();
+            imgPurchasePackage = new Panel();
+            lbPurchasePackage = new Label();
+            lbRegisterSchedule = new Label();
+            flpViewSchedule = new FlowLayoutPanel();
+            imgViewSchedule = new Panel();
+            lbViewSchedule = new Label();
+            borderRegisterMember = new Panel();
+            flpRegisterMember = new FlowLayoutPanel();
+            imgRegisterMember = new Panel();
+            lbRegisterMember = new Label();
+            borderRegisterSchedule = new Panel();
+            flpRegisterSchedule = new FlowLayoutPanel();
+            imgRegisterSchedule = new Panel();
+            borderPurchasePackage = new Panel();
+            borderViewSchedule = new Panel();
+            flpPurchasePackage.SuspendLayout();
+            flpViewSchedule.SuspendLayout();
+            borderRegisterMember.SuspendLayout();
+            flpRegisterMember.SuspendLayout();
+            borderRegisterSchedule.SuspendLayout();
+            flpRegisterSchedule.SuspendLayout();
+            borderPurchasePackage.SuspendLayout();
+            borderViewSchedule.SuspendLayout();
             SuspendLayout();
+            // 
+            // flpPurchasePackage
+            // 
+            flpPurchasePackage.BackColor = Color.FromArgb(230, 230, 230);
+            flpPurchasePackage.Controls.Add(imgPurchasePackage);
+            flpPurchasePackage.Controls.Add(lbPurchasePackage);
+            flpPurchasePackage.Cursor = Cursors.Hand;
+            flpPurchasePackage.Location = new Point(3, 3);
+            flpPurchasePackage.Name = "flpPurchasePackage";
+            flpPurchasePackage.Size = new Size(600, 350);
+            flpPurchasePackage.TabIndex = 1;
+            flpPurchasePackage.MouseLeave += flpPurchasePackagee_MouseLeave;
+            flpPurchasePackage.MouseHover += flpPurchasePackage_MouseHover;
+            // 
+            // imgPurchasePackage
+            // 
+            imgPurchasePackage.BackgroundImage = Properties.Resources.registerMember;
+            imgPurchasePackage.Location = new Point(175, 0);
+            imgPurchasePackage.Margin = new Padding(175, 0, 175, 0);
+            imgPurchasePackage.Name = "imgPurchasePackage";
+            imgPurchasePackage.Size = new Size(250, 250);
+            imgPurchasePackage.TabIndex = 0;
+            imgPurchasePackage.MouseLeave += imgPurchasePackage_MouseLeave;
+            imgPurchasePackage.MouseHover += imgPurchasePackage_MouseHover;
+            // 
+            // lbPurchasePackage
+            // 
+            lbPurchasePackage.AutoSize = true;
+            lbPurchasePackage.Font = new Font("Times New Roman", 25.8000011F, FontStyle.Bold, GraphicsUnit.Point);
+            lbPurchasePackage.ForeColor = Color.FromArgb(59, 41, 55);
+            lbPurchasePackage.Location = new Point(120, 250);
+            lbPurchasePackage.Margin = new Padding(120, 0, 120, 0);
+            lbPurchasePackage.Name = "lbPurchasePackage";
+            lbPurchasePackage.Size = new Size(360, 51);
+            lbPurchasePackage.TabIndex = 1;
+            lbPurchasePackage.Text = "Pruchase Package";
+            lbPurchasePackage.MouseLeave += lbPurchasePackage_MouseLeave;
+            lbPurchasePackage.MouseHover += lbPurchasePackage_MouseHover;
+            // 
+            // lbRegisterSchedule
+            // 
+            lbRegisterSchedule.AutoSize = true;
+            lbRegisterSchedule.Font = new Font("Times New Roman", 25.8000011F, FontStyle.Bold, GraphicsUnit.Point);
+            lbRegisterSchedule.ForeColor = Color.FromArgb(59, 41, 55);
+            lbRegisterSchedule.Location = new Point(122, 250);
+            lbRegisterSchedule.Margin = new Padding(122, 0, 122, 0);
+            lbRegisterSchedule.Name = "lbRegisterSchedule";
+            lbRegisterSchedule.Size = new Size(356, 51);
+            lbRegisterSchedule.TabIndex = 1;
+            lbRegisterSchedule.Text = "Register Schedule";
+            lbRegisterSchedule.MouseLeave += lbRegisterSchedule_MouseLeave;
+            lbRegisterSchedule.MouseHover += lbRegisterSchedule_MouseHover;
+            // 
+            // flpViewSchedule
+            // 
+            flpViewSchedule.BackColor = Color.FromArgb(230, 230, 230);
+            flpViewSchedule.Controls.Add(imgViewSchedule);
+            flpViewSchedule.Controls.Add(lbViewSchedule);
+            flpViewSchedule.Cursor = Cursors.Hand;
+            flpViewSchedule.Location = new Point(3, 3);
+            flpViewSchedule.Name = "flpViewSchedule";
+            flpViewSchedule.Size = new Size(600, 350);
+            flpViewSchedule.TabIndex = 4;
+            flpViewSchedule.MouseLeave += flpViewSchedule_MouseLeave;
+            flpViewSchedule.MouseHover += flpViewSchedule_MouseHover;
+            // 
+            // imgViewSchedule
+            // 
+            imgViewSchedule.BackgroundImage = Properties.Resources.registerMember;
+            imgViewSchedule.Location = new Point(175, 0);
+            imgViewSchedule.Margin = new Padding(175, 0, 175, 0);
+            imgViewSchedule.Name = "imgViewSchedule";
+            imgViewSchedule.Size = new Size(250, 250);
+            imgViewSchedule.TabIndex = 0;
+            imgViewSchedule.MouseLeave += imgViewSchedule_MouseLeave;
+            imgViewSchedule.MouseHover += imgViewSchedule_MouseHover;
+            // 
+            // lbViewSchedule
+            // 
+            lbViewSchedule.AutoSize = true;
+            lbViewSchedule.Font = new Font("Times New Roman", 25.8000011F, FontStyle.Bold, GraphicsUnit.Point);
+            lbViewSchedule.ForeColor = Color.FromArgb(59, 41, 55);
+            lbViewSchedule.Location = new Point(155, 250);
+            lbViewSchedule.Margin = new Padding(155, 0, 155, 0);
+            lbViewSchedule.Name = "lbViewSchedule";
+            lbViewSchedule.Size = new Size(290, 51);
+            lbViewSchedule.TabIndex = 1;
+            lbViewSchedule.Text = "View Schedule";
+            lbViewSchedule.MouseLeave += lbViewSchedule_MouseLeave;
+            lbViewSchedule.MouseHover += lbViewSchedule_MouseHover;
+            // 
+            // borderRegisterMember
+            // 
+            borderRegisterMember.BackColor = Color.White;
+            borderRegisterMember.Controls.Add(flpRegisterMember);
+            borderRegisterMember.Location = new Point(267, 198);
+            borderRegisterMember.Name = "borderRegisterMember";
+            borderRegisterMember.Size = new Size(606, 356);
+            borderRegisterMember.TabIndex = 5;
             // 
             // flpRegisterMember
             // 
-            flpRegisterMember.BackColor = Color.MediumSlateBlue;
-            flpRegisterMember.BorderColor = Color.PaleVioletRed;
-            flpRegisterMember.BorderRadius = 40;
-            flpRegisterMember.BorderSize = 0;
-            flpRegisterMember.ForeColor = Color.White;
-            flpRegisterMember.Location = new Point(248, 197);
-            flpRegisterMember.Margin = new Padding(3, 4, 3, 4);
+            flpRegisterMember.BackColor = Color.FromArgb(230, 230, 230);
+            flpRegisterMember.Controls.Add(imgRegisterMember);
+            flpRegisterMember.Controls.Add(lbRegisterMember);
+            flpRegisterMember.Cursor = Cursors.Hand;
+            flpRegisterMember.Location = new Point(3, 3);
             flpRegisterMember.Name = "flpRegisterMember";
-            flpRegisterMember.Size = new Size(600, 300);
-            flpRegisterMember.TabIndex = 4;
+            flpRegisterMember.Size = new Size(600, 350);
+            flpRegisterMember.TabIndex = 1;
+            flpRegisterMember.MouseLeave += flpRegisterMember_MouseLeave;
+            flpRegisterMember.MouseHover += flpRegisterMember_MouseHover;
             // 
-            // rjFlowLayoutPanel1
+            // imgRegisterMember
             // 
-            rjFlowLayoutPanel1.BackColor = Color.MediumSlateBlue;
-            rjFlowLayoutPanel1.BorderColor = Color.PaleVioletRed;
-            rjFlowLayoutPanel1.BorderRadius = 40;
-            rjFlowLayoutPanel1.BorderSize = 0;
-            rjFlowLayoutPanel1.ForeColor = Color.White;
-            rjFlowLayoutPanel1.Location = new Point(248, 586);
-            rjFlowLayoutPanel1.Margin = new Padding(3, 4, 3, 4);
-            rjFlowLayoutPanel1.Name = "rjFlowLayoutPanel1";
-            rjFlowLayoutPanel1.Size = new Size(600, 300);
-            rjFlowLayoutPanel1.TabIndex = 5;
+            imgRegisterMember.BackgroundImage = Properties.Resources.registerMember;
+            imgRegisterMember.Location = new Point(175, 0);
+            imgRegisterMember.Margin = new Padding(175, 0, 175, 0);
+            imgRegisterMember.Name = "imgRegisterMember";
+            imgRegisterMember.Size = new Size(250, 250);
+            imgRegisterMember.TabIndex = 0;
+            imgRegisterMember.MouseLeave += imgRegisterMember_MouseLeave;
+            imgRegisterMember.MouseHover += imgRegisterMember_MouseHover;
             // 
-            // rjFlowLayoutPanel2
+            // lbRegisterMember
             // 
-            rjFlowLayoutPanel2.BackColor = Color.MediumSlateBlue;
-            rjFlowLayoutPanel2.BorderColor = Color.PaleVioletRed;
-            rjFlowLayoutPanel2.BorderRadius = 40;
-            rjFlowLayoutPanel2.BorderSize = 0;
-            rjFlowLayoutPanel2.ForeColor = Color.White;
-            rjFlowLayoutPanel2.Location = new Point(972, 197);
-            rjFlowLayoutPanel2.Margin = new Padding(3, 4, 3, 4);
-            rjFlowLayoutPanel2.Name = "rjFlowLayoutPanel2";
-            rjFlowLayoutPanel2.Size = new Size(600, 300);
-            rjFlowLayoutPanel2.TabIndex = 6;
+            lbRegisterMember.AutoSize = true;
+            lbRegisterMember.Font = new Font("Times New Roman", 25.8000011F, FontStyle.Bold, GraphicsUnit.Point);
+            lbRegisterMember.ForeColor = Color.FromArgb(59, 41, 55);
+            lbRegisterMember.Location = new Point(124, 250);
+            lbRegisterMember.Margin = new Padding(124, 0, 124, 0);
+            lbRegisterMember.Name = "lbRegisterMember";
+            lbRegisterMember.Size = new Size(352, 51);
+            lbRegisterMember.TabIndex = 1;
+            lbRegisterMember.Text = "Register Member";
+            lbRegisterMember.MouseLeave += lbRegisterMember_MouseLeave;
+            lbRegisterMember.MouseHover += lbRegisterMember_MouseHover;
             // 
-            // rjFlowLayoutPanel3
+            // borderRegisterSchedule
             // 
-            rjFlowLayoutPanel3.BackColor = Color.MediumSlateBlue;
-            rjFlowLayoutPanel3.BorderColor = Color.PaleVioletRed;
-            rjFlowLayoutPanel3.BorderRadius = 40;
-            rjFlowLayoutPanel3.BorderSize = 0;
-            rjFlowLayoutPanel3.ForeColor = Color.White;
-            rjFlowLayoutPanel3.Location = new Point(972, 586);
-            rjFlowLayoutPanel3.Margin = new Padding(3, 4, 3, 4);
-            rjFlowLayoutPanel3.Name = "rjFlowLayoutPanel3";
-            rjFlowLayoutPanel3.Size = new Size(600, 300);
-            rjFlowLayoutPanel3.TabIndex = 5;
+            borderRegisterSchedule.BackColor = Color.White;
+            borderRegisterSchedule.Controls.Add(flpRegisterSchedule);
+            borderRegisterSchedule.Location = new Point(982, 198);
+            borderRegisterSchedule.Name = "borderRegisterSchedule";
+            borderRegisterSchedule.Size = new Size(606, 356);
+            borderRegisterSchedule.TabIndex = 6;
+            // 
+            // flpRegisterSchedule
+            // 
+            flpRegisterSchedule.BackColor = Color.FromArgb(230, 230, 230);
+            flpRegisterSchedule.Controls.Add(imgRegisterSchedule);
+            flpRegisterSchedule.Controls.Add(lbRegisterSchedule);
+            flpRegisterSchedule.Cursor = Cursors.Hand;
+            flpRegisterSchedule.Location = new Point(3, 3);
+            flpRegisterSchedule.Name = "flpRegisterSchedule";
+            flpRegisterSchedule.Size = new Size(600, 350);
+            flpRegisterSchedule.TabIndex = 3;
+            flpRegisterSchedule.MouseLeave += flpRegisterSchedule_MouseLeave;
+            flpRegisterSchedule.MouseHover += flpRegisterSchedule_MouseHover;
+            // 
+            // imgRegisterSchedule
+            // 
+            imgRegisterSchedule.BackgroundImage = Properties.Resources.registerMember;
+            imgRegisterSchedule.Location = new Point(175, 0);
+            imgRegisterSchedule.Margin = new Padding(175, 0, 175, 0);
+            imgRegisterSchedule.Name = "imgRegisterSchedule";
+            imgRegisterSchedule.Size = new Size(250, 250);
+            imgRegisterSchedule.TabIndex = 0;
+            imgRegisterSchedule.MouseLeave += imgRegisterSchedule_MouseLeave;
+            imgRegisterSchedule.MouseHover += imgRegisterSchedule_MouseHover;
+            // 
+            // borderPurchasePackage
+            // 
+            borderPurchasePackage.BackColor = Color.White;
+            borderPurchasePackage.Controls.Add(flpPurchasePackage);
+            borderPurchasePackage.Location = new Point(267, 610);
+            borderPurchasePackage.Name = "borderPurchasePackage";
+            borderPurchasePackage.Size = new Size(606, 356);
+            borderPurchasePackage.TabIndex = 7;
+            // 
+            // borderViewSchedule
+            // 
+            borderViewSchedule.BackColor = Color.White;
+            borderViewSchedule.Controls.Add(flpViewSchedule);
+            borderViewSchedule.Location = new Point(982, 610);
+            borderViewSchedule.Name = "borderViewSchedule";
+            borderViewSchedule.Size = new Size(606, 356);
+            borderViewSchedule.TabIndex = 8;
             // 
             // FHomeUserMenu
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
             ClientSize = new Size(1820, 1080);
             ControlBox = false;
-            Controls.Add(rjFlowLayoutPanel3);
-            Controls.Add(rjFlowLayoutPanel2);
-            Controls.Add(rjFlowLayoutPanel1);
-            Controls.Add(flpRegisterMember);
+            Controls.Add(borderViewSchedule);
+            Controls.Add(borderPurchasePackage);
+            Controls.Add(borderRegisterSchedule);
+            Controls.Add(borderRegisterMember);
             FormBorderStyle = FormBorderStyle.None;
+            Location = new Point(100, 0);
             Margin = new Padding(3, 4, 3, 4);
             Name = "FHomeUserMenu";
             Text = "FHomeUserMenu";
+            flpPurchasePackage.ResumeLayout(false);
+            flpPurchasePackage.PerformLayout();
+            flpViewSchedule.ResumeLayout(false);
+            flpViewSchedule.PerformLayout();
+            borderRegisterMember.ResumeLayout(false);
+            flpRegisterMember.ResumeLayout(false);
+            flpRegisterMember.PerformLayout();
+            borderRegisterSchedule.ResumeLayout(false);
+            flpRegisterSchedule.ResumeLayout(false);
+            flpRegisterSchedule.PerformLayout();
+            borderPurchasePackage.ResumeLayout(false);
+            borderViewSchedule.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
-        private RJFlowLayoutPanel flpRegisterMember;
-        private RJFlowLayoutPanel rjFlowLayoutPanel1;
-        private RJFlowLayoutPanel rjFlowLayoutPanel2;
-        private RJFlowLayoutPanel rjFlowLayoutPanel3;
+        private FlowLayoutPanel flpPurchasePackage;
+        private Panel imgPurchasePackage;
+        private Label lbPurchasePackage;
+        private Label lbRegisterSchedule;
+        private FlowLayoutPanel flpViewSchedule;
+        private Panel imgViewSchedule;
+        private Label lbViewSchedule;
+        private Panel borderRegisterMember;
+        private FlowLayoutPanel flpRegisterMember;
+        private Panel imgRegisterMember;
+        private Label lbRegisterMember;
+        private Panel borderRegisterSchedule;
+        private FlowLayoutPanel flpRegisterSchedule;
+        private Panel imgRegisterSchedule;
+        private Panel borderPurchasePackage;
+        private Panel borderViewSchedule;
     }
 }
--- GymManagementSystem/FHomeUserMenu.cs
+++ GymManagementSystem/FHomeUserMenu.cs
@@ -16,5 +16,133 @@ namespace GymManagementSystem
         {
             InitializeComponent();
         }
+
+        private void imgRegisterMember_MouseHover(object sender, EventArgs e)
+        {
+            borderRegisterMember.BackColor = Color.FromArgb(59, 41, 55);
+        }
+
+        private void lbRegisterMember_MouseHover(object sender, EventArgs e)
+        {
+            borderRegisterMember.BackColor = Color.FromArgb(59, 41, 55);
+        }
+
+        private void flpRegisterMember_MouseHover(object sender, EventArgs e)
+        {
+            borderRegisterMember.BackColor = Color.FromArgb(59, 41, 55);
+        }
+
+        private void imgRegisterMember_MouseLeave(object sender, EventArgs e)
+        {
+            borderRegisterMember.BackColor = Color.FromArgb(255, 255, 255);
+        }
+
+        private void lbRegisterMember_MouseLeave(object sender, EventArgs e)
+        {
+            borderRegisterMember.BackColor = Color.FromArgb(255, 255, 255);
+
+        }
+
+        private void flpRegisterMember_MouseLeave(object sender, EventArgs e)
+        {
+            borderRegisterMember.BackColor = Color.FromArgb(255, 255, 255);
+
+        }
+
+        private void imgRegisterSchedule_MouseHover(object sender, EventArgs e)
+        {
+            borderRegisterSchedule.BackColor = Color.FromArgb(59, 41, 55);
+        }
+
+        private void lbRegisterSchedule_MouseHover(object sender, EventArgs e)
+        {
+            borderRegisterSchedule.BackColor = Color.FromArgb(59, 41, 55);
+        }
+
+        private void flpRegisterSchedule_MouseHover(object sender, EventArgs e)
+        {
+            borderRegisterSchedule.BackColor = Color.FromArgb(59, 41, 55);
+        }
+
+        private void imgRegisterSchedule_MouseLeave(object sender, EventArgs e)
+        {
+            borderRegisterSchedule.BackColor = Color.FromArgb(255, 255, 255);
+        }
+
+        private void lbRegisterSchedule_MouseLeave(object sender, EventArgs e)
+        {
+            borderRegisterSchedule.BackColor = Color.FromArgb(255, 255, 255);
+
+        }
+
+        private void flpRegisterSchedule_MouseLeave(object sender, EventArgs e)
+        {
+            borderRegisterSchedule.BackColor = Color.FromArgb(255, 255, 255);
+
+        }
+
+        private void imgPurchasePackage_MouseHover(object sender, EventArgs e)
+        {
+            borderPurchasePackage.BackColor = Color.FromArgb(59, 41, 55);
+        }
+
+        private void lbPurchasePackage_MouseHover(object sender, EventArgs e)
+        {
+            borderPurchasePackage.BackColor = Color.FromArgb(59, 41, 55);
+        }
+
+        private void flpPurchasePackage_MouseHover(object sender, EventArgs e)
+        {
+            borderPurchasePackage.BackColor = Color.FromArgb(59, 41, 55);
+        }
+
+        private void imgPurchasePackage_MouseLeave(object sender, EventArgs e)
+        {
+            borderPurchasePackage.BackColor = Color.FromArgb(255, 255, 255);
+        }
+
+        private void lbPurchasePackage_MouseLeave(object sender, EventArgs e)
+        {
+            borderPurchasePackage.BackColor = Color.FromArgb(255, 255, 255);
+
+        }
+
+        private void flpPurchasePackagee_MouseLeave(object sender, EventArgs e)
+        {
+            borderPurchasePackage.BackColor = Color.FromArgb(255, 255, 255);
+
+        }
+
+        private void imgViewSchedule_MouseHover(object sender, EventArgs e)
+        {
+            borderViewSchedule.BackColor = Color.FromArgb(59, 41, 55);
+        }
+
+        private void lbViewSchedule_MouseHover(object sender, EventArgs e)
+        {
+            borderViewSchedule.BackColor = Color.FromArgb(59, 41, 55);
+        }
+
+        private void flpViewSchedule_MouseHover(object sender, EventArgs e)
+        {
+            borderViewSchedule.BackColor = Color.FromArgb(59, 41, 55);
+        }
+
+        private void imgViewSchedule_MouseLeave(object sender, EventArgs e)
+        {
+            borderViewSchedule.BackColor = Color.FromArgb(255, 255, 255);
+        }
+
+        private void lbViewSchedule_MouseLeave(object sender, EventArgs e)
+        {
+            borderViewSchedule.BackColor = Color.FromArgb(255, 255, 255);
+
+        }
+
+        private void flpViewSchedule_MouseLeave(object sender, EventArgs e)
+        {
+            borderViewSchedule.BackColor = Color.FromArgb(255, 255, 255);
+
+        }
     }
 }
