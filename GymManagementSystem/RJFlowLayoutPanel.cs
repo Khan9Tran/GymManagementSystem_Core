e884e2efdc6a404157aeb1601d650a95f338c908
thanhhuy25 <nguyenhuythanh256@gmail.com>
2023-10-15 08:53:14 +0700

    Create RJFlowLayoutPanel

---
+++ GymManagementSystem/RJFlowLayoutPanel.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.ComponentModel;
+
+namespace GymManagementSystem
+{
+    public class RJFlowLayoutPanel : FlowLayoutPanel
+    {
+        private int borderSize = 0;
+        private int borderRadius = 40;
+        private Color borderColor = Color.PaleVioletRed;
+
+
+        [Category("Appearance")]
+        [Description("The radius of the button's border.")]
+        public int BorderRadius
+        {
+            get { return borderRadius; }
+            set
+            {
+                borderRadius = value;
+                Invalidate();
+            }
+        }
+
+        [Category("Appearance")]
+        [Description("The radius of the button's border.")]
+        public int BorderSize
+        {
+            get { return borderSize; }
+            set
+            {
+                borderSize = value;
+                Invalidate();
+            }
+        }
+
+        [Category("Appearance")]
+        [Description("The radius of the button's border.")]
+        public Color BorderColor
+        {
+            get { return borderColor; }
+            set
+            {
+                borderColor = value;
+                Invalidate();
+            }
+        }
+        public RJFlowLayoutPanel()
+        {
+            this.Size = new Size(600, 300);
+            this.BackColor = Color.MediumSlateBlue;
+            this.ForeColor = Color.White;
+        }
+        private GraphicsPath GetFigurePath(RectangleF rect, float radius)
+        {
+            GraphicsPath path = new GraphicsPath();
+            path.StartFigure();
+            path.AddArc(rect.X, rect.Y, radius, radius, 180, 90);
+            path.AddArc(rect.Width - radius, rect.Y, radius, radius, 270, 90);
+            path.AddArc(rect.Width - radius, rect.Height - radius, radius, radius, 0, 90);
+            path.AddArc(rect.X, rect.Height - radius, radius, radius, 90, 90);
+            path.CloseFigure();
+
+            return path;
+        }
+
+        protected override void OnPaint(PaintEventArgs pevent)
+        {
+            base.OnPaint(pevent);
+            pevent.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+            RectangleF rectSurface = new RectangleF(0, 0, this.Width, this.Height);
+            RectangleF rectBorder = new RectangleF(1, 1, this.Width - 0.8F, this.Height - 1);
+
+            if (borderRadius > 2)
+            {
+                using (GraphicsPath pathSurface = GetFigurePath(rectSurface, borderRadius))
+                using (GraphicsPath pathBorder = GetFigurePath(rectBorder, borderRadius - 1F))
+                using (Pen penSurface = new Pen(this.Parent.BackColor, 2))
+                using (Pen penBorder = new Pen(borderColor, borderSize))
+                {
+                    penBorder.Alignment = PenAlignment.Inset;
+                    this.Region = new Region(pathSurface);
+                    pevent.Graphics.DrawPath(penSurface, pathSurface);
+
+                    if (borderSize >= 1)
+                    {
+                        pevent.Graphics.DrawPath(penBorder, pathBorder);
+                    }
+                }
+            }
+            else
+            {
+                this.Region = new Region(rectSurface);
+                if (borderSize >= 1)
+                {
+                    using (Pen penBorder = new Pen(borderColor, borderSize))
+                    {
+                        penBorder.Alignment = PenAlignment.Inset;
+                        pevent.Graphics.DrawRectangle(penBorder, 0, 0, this.Width - 1, this.Height - 1);
+                    }
+                }
+            }
+        }
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            base.OnHandleCreated(e);
+            this.Parent.BackColorChanged += new EventHandler(Container_BackColorChanged);
+        }
+
+        private void Container_BackColorChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
