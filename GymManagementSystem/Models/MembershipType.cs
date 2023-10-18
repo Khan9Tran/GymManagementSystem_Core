f378606bcadcc477fd60421626e4bd0785265953
khangdo17 <dokhang1703@gmail.com>
2023-10-18 09:33:13 +0700

    them models

--- GymManagementSystem/Models/MembershipType.cs
+++ GymManagementSystem/Models/MembershipType.cs
@@ -8,5 +8,14 @@ namespace GymManagementSystem.Models
 {
     internal class MembershipType
     {
+        private string iD;
+        private double rate;
+        private string rank;
+
+        public MembershipType() { }
+
+        public string ID { get => iD; set => iD = value; }
+        public double Rate { get => rate; set => rate = value; }
+        public string Rank { get => rank; set => rank = value; }
     }
 }
--- GymManagementSystem/Models/Payment.cs
+++ GymManagementSystem/Models/Payment.cs
@@ -8,5 +8,20 @@ namespace GymManagementSystem.Models
 {
     internal class Payment
     {
+        private string iD;
+        private string memberID;
+        private int height;
+        private Decimal weight;
+        private DateTime date;
+        private string status;
+
+        public Payment() { }
+
+        public string ID { get => iD; set => iD = value; }
+        public string MemberID { get => memberID; set => memberID = value; }
+        public int Height { get => height; set => height = value; }
+        public decimal Weight { get => weight; set => weight = value; }
+        public DateTime Date { get => date; set => date = value; }
+        public string Status { get => status; set => status = value; }
     }
 }
