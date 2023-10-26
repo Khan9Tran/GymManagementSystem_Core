5ceb08382f72cc535ce658e6b397d4f201926ae4
Khan9Tran <trankhangtctv@gmail.com>
2023-10-26 10:15:22 +0700

    thêm btn

--- GymManagementSystem/FHomeUser.Designer.cs
+++ GymManagementSystem/FHomeUser.Designer.cs
@@ -29,19 +29,21 @@
         private void InitializeComponent()
         {
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
-            this.btnMembership = new System.Windows.Forms.Button();
             this.btnHome = new System.Windows.Forms.Button();
+            this.btnMembership = new System.Windows.Forms.Button();
             this.btnBMI = new System.Windows.Forms.Button();
             this.btnLogOut = new System.Windows.Forms.Button();
             this.pnlLoad = new System.Windows.Forms.Panel();
+            this.btnManagement = new System.Windows.Forms.Button();
             this.flowLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // flowLayoutPanel1
             // 
             this.flowLayoutPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(59)))), ((int)(((byte)(41)))), ((int)(((byte)(55)))));
-            this.flowLayoutPanel1.Controls.Add(this.btnHome);
             this.flowLayoutPanel1.Controls.Add(this.btnMembership);
+            this.flowLayoutPanel1.Controls.Add(this.btnHome);
+            this.flowLayoutPanel1.Controls.Add(this.btnManagement);
             this.flowLayoutPanel1.Controls.Add(this.btnBMI);
             this.flowLayoutPanel1.Controls.Add(this.btnLogOut);
             this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
@@ -49,24 +51,6 @@
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
             this.flowLayoutPanel1.Size = new System.Drawing.Size(100, 1080);
             this.flowLayoutPanel1.TabIndex = 0;
-            // 
-            // btnMembership
-            // 
-            this.btnMembership.BackColor = System.Drawing.Color.Transparent;
-            this.btnMembership.BackgroundImage = global::GymManagementSystem.Properties.Resources.vip__1_;
-            this.btnMembership.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.btnMembership.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnMembership.FlatAppearance.BorderSize = 0;
-            this.btnMembership.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(99)))), ((int)(((byte)(63)))), ((int)(((byte)(91)))));
-            this.btnMembership.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(99)))), ((int)(((byte)(63)))), ((int)(((byte)(91)))));
-            this.btnMembership.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnMembership.Location = new System.Drawing.Point(23, 114);
-            this.btnMembership.Margin = new System.Windows.Forms.Padding(23, 27, 23, 0);
-            this.btnMembership.Name = "btnMembership";
-            this.btnMembership.Size = new System.Drawing.Size(60, 60);
-            this.btnMembership.TabIndex = 1;
-            this.btnMembership.UseVisualStyleBackColor = false;
-            this.btnMembership.Click += new System.EventHandler(this.btnMembership_Click);
             // 
             // btnHome
             // 
@@ -78,13 +62,31 @@
             this.btnHome.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(99)))), ((int)(((byte)(63)))), ((int)(((byte)(91)))));
             this.btnHome.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(99)))), ((int)(((byte)(63)))), ((int)(((byte)(91)))));
             this.btnHome.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnHome.Location = new System.Drawing.Point(23, 27);
+            this.btnHome.Location = new System.Drawing.Point(23, 114);
             this.btnHome.Margin = new System.Windows.Forms.Padding(23, 27, 23, 0);
             this.btnHome.Name = "btnHome";
             this.btnHome.Size = new System.Drawing.Size(60, 60);
             this.btnHome.TabIndex = 0;
             this.btnHome.UseVisualStyleBackColor = false;
             this.btnHome.Click += new System.EventHandler(this.btnHome_Click);
+            // 
+            // btnMembership
+            // 
+            this.btnMembership.BackColor = System.Drawing.Color.Transparent;
+            this.btnMembership.BackgroundImage = global::GymManagementSystem.Properties.Resources.vip__1_;
+            this.btnMembership.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnMembership.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnMembership.FlatAppearance.BorderSize = 0;
+            this.btnMembership.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(99)))), ((int)(((byte)(63)))), ((int)(((byte)(91)))));
+            this.btnMembership.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(99)))), ((int)(((byte)(63)))), ((int)(((byte)(91)))));
+            this.btnMembership.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMembership.Location = new System.Drawing.Point(23, 27);
+            this.btnMembership.Margin = new System.Windows.Forms.Padding(23, 27, 23, 0);
+            this.btnMembership.Name = "btnMembership";
+            this.btnMembership.Size = new System.Drawing.Size(60, 60);
+            this.btnMembership.TabIndex = 1;
+            this.btnMembership.UseVisualStyleBackColor = false;
+            this.btnMembership.Click += new System.EventHandler(this.btnMembership_Click);
             // 
             // btnBMI
             // 
@@ -96,7 +98,7 @@
             this.btnBMI.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(99)))), ((int)(((byte)(63)))), ((int)(((byte)(91)))));
             this.btnBMI.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(99)))), ((int)(((byte)(63)))), ((int)(((byte)(91)))));
             this.btnBMI.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnBMI.Location = new System.Drawing.Point(23, 201);
+            this.btnBMI.Location = new System.Drawing.Point(23, 288);
             this.btnBMI.Margin = new System.Windows.Forms.Padding(23, 27, 23, 0);
             this.btnBMI.Name = "btnBMI";
             this.btnBMI.Size = new System.Drawing.Size(60, 60);
@@ -113,7 +115,7 @@
             this.btnLogOut.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(99)))), ((int)(((byte)(63)))), ((int)(((byte)(91)))));
             this.btnLogOut.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(99)))), ((int)(((byte)(63)))), ((int)(((byte)(91)))));
             this.btnLogOut.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnLogOut.Location = new System.Drawing.Point(23, 288);
+            this.btnLogOut.Location = new System.Drawing.Point(23, 375);
             this.btnLogOut.Margin = new System.Windows.Forms.Padding(23, 27, 23, 0);
             this.btnLogOut.Name = "btnLogOut";
             this.btnLogOut.Size = new System.Drawing.Size(60, 60);
@@ -127,6 +129,23 @@
             this.pnlLoad.Name = "pnlLoad";
             this.pnlLoad.Size = new System.Drawing.Size(1820, 1080);
             this.pnlLoad.TabIndex = 1;
+            // 
+            // btnManagement
+            // 
+            this.btnManagement.BackColor = System.Drawing.Color.Transparent;
+            this.btnManagement.BackgroundImage = global::GymManagementSystem.Properties.Resources.folder;
+            this.btnManagement.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnManagement.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnManagement.FlatAppearance.BorderSize = 0;
+            this.btnManagement.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(99)))), ((int)(((byte)(63)))), ((int)(((byte)(91)))));
+            this.btnManagement.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(99)))), ((int)(((byte)(63)))), ((int)(((byte)(91)))));
+            this.btnManagement.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnManagement.Location = new System.Drawing.Point(23, 201);
+            this.btnManagement.Margin = new System.Windows.Forms.Padding(23, 27, 23, 0);
+            this.btnManagement.Name = "btnManagement";
+            this.btnManagement.Size = new System.Drawing.Size(60, 60);
+            this.btnManagement.TabIndex = 5;
+            this.btnManagement.UseVisualStyleBackColor = false;
             // 
             // FHomeUser
             // 
@@ -152,5 +171,6 @@
         private Button btnMembership;
         private Button btnBMI;
         private Button btnLogOut;
+        private Button btnManagement;
     }
 }
--- GymManagementSystem/Properties/Resources.Designer.cs
+++ GymManagementSystem/Properties/Resources.Designer.cs
@@ -123,6 +123,16 @@ namespace GymManagementSystem.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap folder {
+            get {
+                object obj = ResourceManager.GetObject("folder", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap gym {
             get {
                 object obj = ResourceManager.GetObject("gym", resourceCulture);
