f3f844b0318061afcbf190542411d887ba23aaec
khangdo17 <dokhang1703@gmail.com>
2023-10-31 15:12:27 +0700

    fix description, duration FWorkout

--- GymManagementSystem.sql
+++ GymManagementSystem.sql
@@ -1604,8 +1604,8 @@ CREATE PROCEDURE PROC_AddWorkout
     @ID CHAR(6),
     @Name NVARCHAR(50),
     @Type VARCHAR(50),
-    @Description VARCHAR,
-    @Duration CHAR
+    @Description NTEXT,
+    @Duration INT
 AS
 BEGIN
 	IF EXISTS (SELECT * FROM WorkOut WHERE [Name] = @Name)
@@ -1632,8 +1632,8 @@ CREATE PROCEDURE PROC_UpdateWorkout
 	@ID char(6),
     @Name NVARCHAR(50),
     @Type VARCHAR(50),
-    @Description VARCHAR,
-    @Duration CHAR
+    @Description NTEXT,
+    @Duration INT
 AS
 BEGIN
 		UPDATE WorkOut
